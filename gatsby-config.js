edc73b9664ff610a87c90b0ba73fc1fc4c5755b0
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-11-17 18:03:31 +0700

    Update plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,6 +126,7 @@ module.exports = {
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
           'gatsby-remark-external-links',
+          `gatsby-plugin-remove-fingerprints`,
         ],
       },
     },
