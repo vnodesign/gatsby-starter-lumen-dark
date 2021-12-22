14f92fa52350e896701b33d5abad692fa532bf82
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-12-22 21:56:39 +0700

    Upgrade test

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,5 @@ const Shell = require('child_process');
 exports.createPages = require('./gatsby/create-pages.js');
 exports.onCreateNode = require('./gatsby/on-create-node.js');
 exports.onPostBuild = function () {
-    Shell.execSync('cp src/ads.txt public');
+    Shell.execSync('cp ads.txt public');
   };
