5f8ff3d29779915aef2acb635a02ec1bed16e10d
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-12-31 01:05:45 +0700

    Fix no display ga

--- src/html.js
+++ src/html.js
@@ -9,12 +9,12 @@ export default function HTML(props) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         {props.headComponents}
-        <script strategy="lazyOnload" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063" />
       </head>
       <body {...props.bodyAttributes} itemScope itemType="https://schema.org/WebPage">
         {props.preBodyComponents}
         <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
+        <script dangerouslySetInnerHTML={{__html: `function td_adsense(){var t=document.createElement("script");t.async=!0,t.defer=!0,t.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4183134625750063",document.body.appendChild(t)}window.addEventListener?window.addEventListener("load",td_adsense,!1):window.attachEvent?window.attachEvent("onload",td_adsense):window.onload=td_adsense;`,}} />
       </body>
     </html>
   )
