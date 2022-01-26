2a0700e5918d163aefca0ff1dd2c8f83c0a5637c
Tuan Duc Designer <nguoisoidangyeu@gmail.com>
2022-01-26 16:26:46 +0700

    Add slug from meta tag og:url

--- src/templates/categories-list-template.tsx
+++ src/templates/categories-list-template.tsx
@@ -8,16 +8,14 @@ import Layout from "../components/Layout";
 import Page from "../components/Page";
 import { useSiteMetadata, useCategoriesList } from "../hooks";
 
-const CategoriesListTemplate = ({location}) => {
+const CategoriesListTemplate = () => {
   const { title, subtitle } = useSiteMetadata();
   const categories = useCategoriesList();
-  const baseUrl = location.href ? location.href : '';
 
   return (
     <Layout title={`Categories - ${title}`} description={subtitle}>
       <Helment>
       <meta property="og:type" content="article"/>
-      <meta property="og:url" content={baseUrl} />
       <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
       </Helment>
       <Sidebar />
