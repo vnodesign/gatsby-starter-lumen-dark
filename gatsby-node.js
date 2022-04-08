a7b5ddc2b1960726c2cbb813ebd02349cec70152
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2022-04-09 01:01:13 +0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,3 @@
 'use strict';
-const Shell = require('child_process');
 exports.createPages = require('./gatsby/create-pages.js');
 exports.onCreateNode = require('./gatsby/on-create-node.js');
-exports.onPostBuild = () => {
-    Shell.execSync('cp ads.txt public/');
-    Shell.execSync('cp -r backend public/');
-  };
